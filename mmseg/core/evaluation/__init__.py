7130f57b3fdd72426b32dcb6bcba4a9b5bb50707
DingZY <91234876+gitdzy@users.noreply.github.com>
2024-03-16 00:18:21 +0800

    Update __init__.py

--- mmseg/core/evaluation/__init__.py
+++ mmseg/core/evaluation/__init__.py
@@ -4,7 +4,7 @@ from .class_names import get_classes, get_palette
 from .eval_hooks import DistEvalHook, EvalHook
 from .metrics import eval_metrics, mean_dice, mean_fscore, mean_iou
 
-from .lesion_metric import metrics
+from .lesion_metric import lesion_metrics
 
 __all__ = [
     'EvalHook', 'DistEvalHook', 'mean_dice', 'mean_iou', 'mean_fscore',
