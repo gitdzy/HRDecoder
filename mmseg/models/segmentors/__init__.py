69ed5f9017431b89f748e780b1e847fee78d9ef9
DingZY <91234876+gitdzy@users.noreply.github.com>
2024-03-16 01:01:50 +0800

    Delete mmseg directory

--- mmseg/models/segmentors/__init__.py
+++
@@ -1,9 +0,0 @@
-
-from .base import BaseSegmentor
-from .encoder_decoder import EncoderDecoder
-
-from .lesion_encoder_decoder import LesionEncoderDecoder
-
-from .HRDecoder import HRDecoder,EfficientHRDecoder
-
-__all__ = ['BaseSegmentor', 'EncoderDecoder']
