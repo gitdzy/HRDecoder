1f691846eb4bef6a29090cfd4edb1afa3ca7b969
DingZY <91234876+gitdzy@users.noreply.github.com>
2024-03-23 09:03:37 +0800

    Update efficient-hrdecoder_fcn_hr48_idrid_2880x1920-slide.py

--- configs/lesion/efficient-hrdecoder_fcn_hr48_idrid_2880x1920-slide.py
+++ configs/lesion/efficient-hrdecoder_fcn_hr48_idrid_2880x1920-slide.py
@@ -32,7 +32,7 @@ data=dict(
 )
 runner = dict(type='IterBasedRunner', max_iters=20000)
 checkpoint_config = dict(by_epoch=False, interval=1000, max_keep_ckpts=1)
-evaluation = dict(interval=20000, 
+evaluation = dict(interval=4000, 
                   metric='mIoU',
                   priority='LOW',
                   save_best='mIoU')
